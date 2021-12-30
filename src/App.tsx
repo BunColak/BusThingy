754457a387d57236f27a486a9e6a1427218ba287
Bün Colak <bun.colak@motius.de>
2021-12-30 20:57:14 +0300

    electron mapping

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,68 @@
-import React from 'react'
+import React from "react";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  AppBar,
+  Box,
+  Container,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import mappings from "./mappings.json";
+import { ExpandMore as ExpandMoreIcon, Send } from "@mui/icons-material";
+const cp = require("child_process");
 
 const App = () => {
-  return (
-    <div>
-      Hello there
-    </div>
-  )
-}
+  const openFile = async (file: string) => {
+    const child = cp.execFile(file, [], (error: any, stdout: any, stderr: any) => {
+      if (error) {
+        console.log(error, stderr);
 
-export default App
+        throw error;
+      }
+      console.log(stdout);
+    });
+    console.log(child);
+  };
+
+  return (
+    <Box>
+      <AppBar position="static">
+        <Toolbar color="primary">
+          <Typography variant="h5">Airbus Thingy</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container>
+        <Box mt={4}>
+          {mappings.map((mapping) => (
+            <Accordion key={mapping.name}>
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography variant="h5">{mapping.name}</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <List>
+                  {mapping.lessons.map((lesson) => (
+                    <ListItem button onClick={() => openFile(lesson.path)}>
+                      <ListItemText
+                        primary={lesson.name}
+                        secondary="something here"
+                      />
+                      <Send color="action" />
+                    </ListItem>
+                  ))}
+                </List>
+              </AccordionDetails>
+            </Accordion>
+          ))}
+        </Box>
+      </Container>
+    </Box>
+  );
+};
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import theme from "./theme";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
